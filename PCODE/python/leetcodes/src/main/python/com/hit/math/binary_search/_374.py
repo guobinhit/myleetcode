a70554a2ea0b4021c031268d23367930e48179cb
Charies Gavin <guobinhit@gmail.com>
2018-12-18 13:36:10 +0800

    69 & 374 python version

---
+++ PCODE/python/leetcodes/src/main/python/com/hit/math/binary_search/_374.py
@@ -0,0 +1,45 @@
+"""
+374. Guess Number Higher or Lower
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+Example :
+
+Input: n = 10, pick = 6
+Output: 6
+"""
+
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 0
+        right = n
+        while left <= right:
+            mid = (left + right) // 2
+            """ wirte as Solution.guess way is in order to defend error tips """
+            cmp = Solution.guess(mid)
+            if cmp == 0:
+                return mid
+            elif cmp == 1:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+    """ Below function is not real guess """
+
+    def guess(self, num):
+        return 1
---
+++ PCODE/python/leetcodes/src/main/python/com/hit/math/binary_search/_69.py
@@ -0,0 +1,42 @@
+"""
+69. Sqrt(x)
+
+Implement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+
+Input: 4
+Output: 2
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since
+             the decimal part is truncated, 2 is returned.
+"""
+
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+
+        left = 1
+        right = x
+        while True:
+            mid = left + (right - left) // 2
+            if mid > x / mid:
+                right = mid - 1
+            else:
+                if mid + 1 > x / (mid + 1):
+                    return mid
+                else:
+                    left = mid + 1
