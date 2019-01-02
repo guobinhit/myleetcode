ed9a0ea8d54bf8913d36c3f68bf074448234bd69
Charies Gavin <guobinhit@gmail.com>
2019-01-02 17:02:00 +0800

    add code

--- PCODE/java/leetcodes/src/test/java/com/hit/math/TestCase.java
+++ PCODE/java/leetcodes/src/test/java/com/hit/math/TestCase.java
@@ -13,15 +13,32 @@ import java.math.BigInteger;
  */
 public class TestCase {
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        int[] arr1 = {1};
-        CommonUtils.printIntArray(arr);
-        buddleSort(arr);
-        CommonUtils.printIntArray(arr);
+        int[] arr = {1,2,3,1};
+//        int[] arr1 = {1};
+//        CommonUtils.printIntArray(arr);
+//        buddleSort(arr);
+//        CommonUtils.printIntArray(arr);
+
+        System.out.println(findPeakElement(arr));
     }
 
+    private static int findPeakElement(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i - 1] < nums[i] &&
+                    nums[i + 1] < nums[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
     private static void buddleSort(int[] arr) {
-        for (int i = arr.length -1; i > 0; i--) {
+        for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j + 1] < arr[j]) {
                     int temp = arr[j];
