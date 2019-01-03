bd866fd3d357be6f3c2763894d221041f1e96f72
Charies Gavin <guobinhit@gmail.com>
2019-01-03 10:50:06 +0800

    162

---
+++ PCODE/java/leetcodes/src/main/java/com/hit/math/binary_search/_162.java
@@ -0,0 +1,47 @@
+package com.hit.math.binary_search;
+
+/**
+ * 162. Find Peak Element
+ * <p>
+ * A peak element is an element that is greater than its neighbors.
+ * <p>
+ * Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+ * <p>
+ * The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+ * <p>
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,1]
+ * <p>
+ * Output: 2
+ * <p>
+ * Explanation: 3 is a peak element and your function should return the index number 2.
+ * Example 2:
+ * <p>
+ * Input: nums = [1,2,1,3,5,6,4]
+ * <p>
+ * Output: 1 or 5
+ * <p>
+ * Explanation: Your function can return either index number 1 where the peak element is 2,
+ * or index number 5 where the peak element is 6.
+ * <p>
+ * Note:
+ * <p>
+ * Your solution should be in logarithmic complexity.
+ */
+public class _162 {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
