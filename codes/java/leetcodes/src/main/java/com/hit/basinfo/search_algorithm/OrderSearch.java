624af1ddfbcd56ceea5ca081f8584412044cad6c
Charies Gavin <guobinhit@gmail.com>
2019-11-10 11:26:08 +0800

    delete a unuseful method of OrderSearch

--- codes/java/leetcodes/src/main/java/com/hit/basinfo/search_algorithm/OrderSearch.java
+++ codes/java/leetcodes/src/main/java/com/hit/basinfo/search_algorithm/OrderSearch.java
@@ -36,42 +36,4 @@ public class OrderSearch {
         }
         return -1;
     }
-
-    /**
-     * Order Search(Advanced Edition)
-     *
-     * @param nums   pending query array
-     * @param target target element
-     * @return target element index
-     */
-    public int orderSearch2(int[] nums, int target) {
-        // set a temp array, length is nums.length + 1, in order to store sentry
-        int[] temp = new int[nums.length + 1];
-
-        // store all elements in the array nums in the temp array
-        for (int i = 0; i < nums.length; i++) {
-            temp[i] = nums[i];
-        }
-
-        // set up sentry
-        temp[nums.length] = target;
-        int result;
-        for (int i = 0; ; i++) {
-            if (temp[i] == target) {
-                result = i;
-                // If the above conditions are met, the cycle is terminated
-                break;
-            }
-        }
-
-        /**
-         * If the result value is less than the length of the nums array,
-         * it must be the target element index
-         */
-        if (result < nums.length) {
-            return result;
-        } else {
-            return -1;
-        }
-    }
 }
--- codes/python/leetcodes/src/main/python/com/hit/basinfo/search_algorithm/OrderSearch.py
+++ codes/python/leetcodes/src/main/python/com/hit/basinfo/search_algorithm/OrderSearch.py
@@ -19,27 +19,3 @@ def order_search(nums, target):
         index += 1
 
     return -1
-
-
-def order_search_2(nums, target):
-    if len(nums) == 0:
-        return -1
-    elif len(nums) < 2 and nums[0] != target:
-        return -1
-
-    nums2 = []
-    for num in nums:
-        nums2.append(num)
-
-    nums2.append(target)
-
-    index = 0
-    while index < len(nums):
-        if nums2[index] == target:
-            break
-        index += 1
-
-    if index < len(nums):
-        return index
-    else:
-        return -1
