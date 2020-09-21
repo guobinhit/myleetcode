8c87698a78a8e8fde901745a9f02383183bfad10
Charies Gavin <guobinhit@qq.com>
2020-09-21 10:41:21 +0800

    optimize solution of 3 & 49

--- codes/java/leetcodes/src/main/java/com/hit/basmath/learn/hash_table/_3.java
+++ codes/java/leetcodes/src/main/java/com/hit/basmath/learn/hash_table/_3.java
@@ -32,75 +32,22 @@ import java.util.Set;
  */
 public class _3 {
     /**
-     * Solution 1: Violence method
+     * Slide window method of optimize
      *
-     * @param s
-     * @return
+     * @param s target string
+     * @return size
      */
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j <= n; j++) {
-                if (allUnique(s, i, j)) {
-                    ans = Math.max(ans, j - i);
-                }
-            }
-        }
-        return ans;
-    }
-
-    private boolean allUnique(String s, int start, int end) {
-        Set<Character> aSet = new HashSet<>();
-        for (int i = start; i < end; i++) {
-            if (aSet.contains(s.charAt(i))) {
-                return false;
-            } else {
-                aSet.add(s.charAt(i));
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Solution 2: Slide window method
-     *
-     * @param s
-     * @return
-     */
-    public int lengthOfLongestSubstring2(String s) {
-        int n = s.length();
-        Set<Character> set = new HashSet<>();
-        int ans = 0, i = 0, j = 0;
-        while (i < n && j < n) {
-            // try to extend the range [i, j]
-            if (!set.contains(s.charAt(j))) {
-                set.add(s.charAt(j++));
-                ans = Math.max(ans, j - i);
-            } else {
-                set.remove(s.charAt(i++));
-            }
-        }
-        return ans;
-    }
-
-    /**
-     * Solution 3: Slide window method of optimize
-     *
-     * @param s
-     * @return
-     */
-    public int lengthOfLongestSubstring3(String s) {
-        int n = s.length(), ans = 0;
+        int length = s.length(), ans = 0;
         // current index of character
         Map<Character, Integer> map = new HashMap<>();
         // try to extend the range [i, j]
-        for (int j = 0, i = 0; j < n; j++) {
-            if (map.containsKey(s.charAt(j))) {
-                i = Math.max(map.get(s.charAt(j)), i);
+        for (int i = 0, j = 0; i < length; i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(map.get(s.charAt(i)), j);
             }
-            ans = Math.max(ans, j - i + 1);
-            map.put(s.charAt(j), j + 1);
+            ans = Math.max(ans, i - j + 1);
+            map.put(s.charAt(i), i + 1);
         }
         return ans;
     }
--- codes/java/leetcodes/src/main/java/com/hit/basmath/learn/hash_table/_49.java
+++ codes/java/leetcodes/src/main/java/com/hit/basmath/learn/hash_table/_49.java
@@ -26,15 +26,17 @@ import java.util.*;
  */
 public class _49 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) return new ArrayList<List<String>>();
+        if (strs == null || strs.length == 0) return new ArrayList<>();
         Map<String, List<String>> map = new HashMap<String, List<String>>();
         for (String s : strs) {
             char[] ca = s.toCharArray();
             Arrays.sort(ca);
             String keyStr = String.valueOf(ca);
-            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<String>());
+            if (!map.containsKey(keyStr)) {
+                map.put(keyStr, new ArrayList<>());
+            }
             map.get(keyStr).add(s);
         }
-        return new ArrayList<List<String>>(map.values());
+        return new ArrayList<>(map.values());
     }
 }
