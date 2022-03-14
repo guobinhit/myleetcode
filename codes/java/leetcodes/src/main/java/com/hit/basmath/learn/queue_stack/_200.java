9470a54243999db9826b2647a1f3a09ac74bd179
Charies Gavin <guobinhit@qq.com>
2022-03-14 20:25:41 +0800

    Update _200.java

--- codes/java/leetcodes/src/main/java/com/hit/basmath/learn/queue_stack/_200.java
+++ codes/java/leetcodes/src/main/java/com/hit/basmath/learn/queue_stack/_200.java
@@ -28,60 +28,28 @@ package com.hit.basmath.learn.queue_stack;
  * Output: 3
  */
 public class _200 {
-    private static int gridLenght;
-    private static int gridDepth;
-
-    public static int numIslands(char[][] grid) {
-        /**
-         * Record island number
-         */
-        int count = 0;
-        gridLenght = grid.length;
-        if (gridLenght == 0) {
-            return 0;
-        }
-        gridDepth = grid[0].length;
-        for (int i = 0; i < gridLenght; i++) {
-            for (int j = 0; j < gridDepth; j++)
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) return 0;
+         int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++)
                 if (grid[i][j] == '1') {
                     dfsMark(grid, i, j);
-                    ++count;
+                    count++;
                 }
         }
         return count;
     }
 
-    private static void dfsMark(char[][] grid, int i, int j) {
-        /**
-         * When we are meet 4 edges of grid, return immediately
-         */
-        if (i < 0 || j < 0 || i >= gridLenght || j >= gridDepth || grid[i][j] != '1') {
+    private void dfsMark(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || 
+                j >= grid[0].length || grid[i][j] != '1') {
             return;
         }
         grid[i][j] = '0';
-        /**
-         * This 4 iteration is handle current element's 4 edges
-         */
         dfsMark(grid, i + 1, j);
         dfsMark(grid, i - 1, j);
         dfsMark(grid, i, j + 1);
         dfsMark(grid, i, j - 1);
-    }
-
-    public static void main(String[] args) {
-        char[][] test1 = {
-                {'1', '1', '1', '1', '0'},
-                {'1', '1', '0', '1', '0'},
-                {'1', '1', '0', '0', '0'},
-                {'0', '0', '0', '0', '0'}
-        };
-        char[][] test2 = {
-                {'1', '1', '0', '0', '0'},
-                {'1', '1', '0', '0', '0'},
-                {'0', '0', '1', '0', '0'},
-                {'0', '0', '0', '1', '1'}
-        };
-        System.out.println("In this grid, island's number is " + _200.numIslands(test1));
-        System.out.println("In this grid, island's number is " + _200.numIslands(test2));
     }
 }
